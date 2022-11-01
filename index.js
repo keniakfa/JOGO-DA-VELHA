28b72d35c237ce823f6cac37f6c61adb556ec33e
Kênia Araujo <kenia.kfa@kenia-air.mynetworksettings.com>
2022-11-01 12:32:23 -0500

    Done

--- index.js
+++ index.js
@@ -1,58 +1,103 @@
 const cells = document.querySelectorAll(".cell");
-const statusText= document.querySelector("statusText")
-const restartBtn= document.querySelector("restartBtn")
+const statusText= document.querySelector("#statusText");
+const restartBtn= document.querySelector("#restartBtn");
 
-const winConditions= [
-[0,1,2],
-[3,4,5],
+
+function changeColor(id){
+ document.querySelector('body').style.background=id 
+}
+
+
+const winConditions = [
+[0,1,2],          
+[3,4,5],       
 [6,7,8],
-[0,3,6],
+[0,3,6],          
 [1,4,7],
 [2,5,8],
 [0,4,8],
 [2,4,6],
 ];
 
-let options= ["","","","","","","",""];
-let currentPlayer="X";
-const running= false;          //tracking the game 
+let options = ["","","","","","","","",""];
+let currentPlayer = "X";
+let running = true;
 
 startGame();
+
 function startGame(){
-    cells.forEach(cell => cell.addEventListener("click",cellClicked));  //CALLBACK - Done
-    restartBtn.addEventListener("click");                 //restartGame
-    console.log(startGame)
-    statusText.textContent= `${currentPlayers turn}`;  //These are the currentPlayers turn
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));  
+    restartBtn.addEventListener("click", restartGame);                 
+    statusText.textContent= `${currentPlayer}'s turn`; 
     
+   
 
 }
 
 function cellClicked(){
-    const cellIndex = this.getAttribute("cellIndex");    //Cada cell que eu clicar/ Attribute -cellIndex HTML
+   
+    const cellIndex = this.getAttribute("cellIndex");     
 
-    If(options[cellIndex] != "" || !running){ //Index of cell ...index does not equal an empty space
-        return;
-    }
+    if(options[cellIndex] != "" || !running) return;
+    
+       
+
     updateCell(this,cellIndex);
     checkWinner();
 }
 
+
 function updateCell(cell, index){
-    option[index] = currentPlayer;
-    cell.textContent = currentPlayer;   //x
+    console.log(options)
+    
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;   
+}
 
 function changedPlayer(){
-    currentPlayer = (currentPlayer == "X") ? "0" : "X" // Condition is if change player is equal to X
-    //if cuurent player is equal i need reassign our current player with O otherwise X
-    statusText.textContent = `${currentPlayer}`
-     
+    currentPlayer = (currentPlayer === "X") ? "0" : "X";
+    statusText.textContent = `${currentPlayer}'s turn'`;
+    
+
 }
 
 function checkWinner(){
-    let roundWon =false;
+    let roundWon = false;
+    
 
+    for(let i= 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+   
+    
+        if(cellA === "" || cellB === "" || cellC === ""){
+            continue; 
+          
+            roundWon = false;
+        } else if((cellA === cellB) && (cellB === cellC)){ 
+            roundWon = true;
+        
+        }                                  
+    }
+ 
+    if(roundWon){ 
+        statusText.textContent = `${currentPlayer} wins!`
+        running =false;
+    } else if(!options.includes("")){
+        statusText.textContent = "Draw!";
+    } else{
+        changedPlayer();
+    
+    }
 }
 
 function restartGame(){
-
+    currentPlayer = "X"
+    options = ["","","","","","","","",""];
+    statusText.textContent = `${currentPlayer}'s turn'`
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+  
 }
